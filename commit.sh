350e46822f40ccafa3928e871973687440d3b576
Daiki Morita <daikimorita@DaikinoMacBook-Air.local>
2020-12-10 21:53:44 +0900

    修正

--- commit.sh
+++ commit.sh
@@ -3,3 +3,10 @@
 git add .
 git commit -m "修正"
 git push origin master
+
+PF=$(cat .credentials | grep ^# -2 | tail -n 1)
+expect -c "
+set timeout 5
+spawn git push origin master
+expect \"Enter passphrase for key '/Users/daikimorita/.ssh/id_rsa':\"
+"
