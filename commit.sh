9a0274c9b83cd194c28a6414ab7fc4182f2fb015
DaikiMorita <d.morita@outlook.com>
2020-12-11 03:58:19 +0900

    修正

--- commit.sh
+++ commit.sh
@@ -19,10 +19,8 @@ conda activate resume
 
 # 更新日を書き換え
 DATE=$(python3 replace_date.py)
-
 figlet -f standard $DATE
 
-echo "----------------------------------------"
 git diff --name-only
 echo "----------------------------------------"
 echo ""
