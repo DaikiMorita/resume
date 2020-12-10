b41037cecb6026aff7bc8de93f1fe2200617b5d3
Daiki Morita <daikimorita@DaikinoMacBook-Air.local>
2020-12-10 22:12:38 +0900

    修正

--- commit.sh
+++ commit.sh
@@ -3,7 +3,6 @@
 git add .
 git commit -m "修正"
 PF=$(cat .credentials | grep PF -2 | tail -n 1)
-echo $PF
 expect -c "
 set timeout 5
 spawn git push origin master
@@ -12,4 +11,11 @@ send \"${PF}\n\"
 expect \"master -> master\"
 exit 0
 "
-echo https://daikimorita.github.io/resume/
+echo "  ____                _     __         _  _          _   _                                 _            _ ";
+echo " |  _ \   ___   _ __ | |_  / _|  ___  | |(_)  ___   | | | | _ __    __ _  _ __   __ _   __| |  ___   __| | ";
+echo " | |_) | / _ \ | '__|| __|| |_  / _ \ | || | / _ \  | | | || '_ \  / _` || '__| / _` | / _` | / _ \ / _` | ";
+echo " |  __/ | (_) || |   | |_ |  _|| (_) || || || (_) | | |_| || |_) || (_| || |   | (_| || (_| ||  __/| (_| | ";
+echo " |_|     \___/ |_|    \__||_|   \___/ |_||_| \___/   \___/ | .__/  \__, ||_|    \__,_| \__,_| \___| \__,_| ";
+echo "                                                           |_|     |___/ ";
+echo "";
+echo https://daikimorita.github.io/resume/;
