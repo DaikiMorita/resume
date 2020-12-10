c6bf60a465f2be0c79486d5d98fa7d2da66f5095
Daiki Morita <daikimorita@DaikinoMacBook-Air.local>
2020-12-11 03:47:22 +0900

    修正

--- commit.sh
+++ commit.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-conda init bash
+# >>> conda init >>>
+__conda_setup="$(CONDA_REPORT_ERRORS=false '$HOME/anaconda3/bin/conda' shell.bash hook 2>/dev/null)"
+if [ $? -eq 0 ]; then
+  \eval "$__conda_setup"
+else
+  if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+    . "$HOME/anaconda3/etc/profile.d/conda.sh"
+    CONDA_CHANGEPS1=false conda activate base
+  else
+    \export PATH="$PATH:$HOME/anaconda3/bin"
+  fi
+fi
+unset __conda_setup
+# <<< conda init <<<
+
 conda activate resume
 python3 replace_date.py
 
@@ -38,3 +52,5 @@ echo "☆～｡;:ﾟ*～｡;:ﾟ*～｡;:ﾟ*～｡;:ﾟ*～☆☆～｡;:ﾟ*�
 echo ""
 echo "Go to Github Pages >> https://daikimorita.github.io/resume"
 echo ""
+
+conda deactivate
