2e59d30df2ec7ffdf5653d63fbf6e9719f4376b4
DaikiMorita <d.morita@outlook.com>
2021-01-22 03:55:43 +0900

    修正

--- pyscript/replace_date.py
+++ pyscript/replace_date.py
@@ -1,5 +1,7 @@
 import bs4
 from datetime import datetime as dt
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 target_html = 'index.html'
 
@@ -12,6 +14,7 @@ with open(target_html, mode='r') as f:
     html = f.read()
 
 soup = bs4.BeautifulSoup(html, 'html.parser')
+
 new_tag = soup.new_tag('p', attrs={'id': 'update_date'})
 new_tag.string = f'最終更新日：{year}年{month}月{day}日'
 soup.find('p', id="update_date").replace_with(new_tag)
@@ -19,4 +22,11 @@ soup.find('p', id="update_date").replace_with(new_tag)
 with open(target_html, mode='w') as f:
     f.write((soup.prettify(formatter='html5')))
 
-print(f'{year} / {month} / {day}')
+print(f'{year}/{month}/{day}')
+
+new_tag = soup.new_tag('p', attrs={'id': 'birthday'})
+age = relativedelta(today, date(1993, 8, 25)).years
+new_tag.string = f'{age}歳'
+soup.find('p', id="birthday").replace_with(new_tag)
+
+print(age)
