5027bcbb37d8c83abb6aee0c9ecfbb45f4f99a7d
DaikiMorita <d.morita@outlook.com>
2021-01-22 04:06:04 +0900

    修正

--- pyscript/replace_date.py
+++ pyscript/replace_date.py
@@ -19,12 +19,12 @@ new_tag = soup.new_tag('p', attrs={'id': 'update_date'})
 new_tag.string = f'最終更新日：{year}年{month}月{day}日'
 soup.find('p', id="update_date").replace_with(new_tag)
 
-with open(target_html, mode='w') as f:
-    f.write((soup.prettify(formatter='html5')))
-
 print(f'{year}/{month}/{day}')
 
 new_tag = soup.new_tag('p', attrs={'id': 'birthday'})
 age = relativedelta(today, date(1993, 8, 25)).years + 1
-new_tag.string = f'{age}歳'
+new_tag.string = f'年齢：{age}歳'
 soup.find('p', id="birthday").replace_with(new_tag)
+
+with open(target_html, mode='w') as f:
+    f.write((soup.prettify(formatter='html5')))
